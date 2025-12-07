89c8f4f237eeb4d2cef7a21f94b75c9f24039a1b
mai kieu <mai.kieu@hva.nl>
2025-12-08 00:28:45 +0100

    Deploy to GitHub Pages

---
+++ src/components/content/projects/ProjectModal.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+name: "ProjectModal"
+})
+</script>
+
+<template>
+  $END$
+</template>
+
+<style scoped>
+
+</style>
